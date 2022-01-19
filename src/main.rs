5b5092103550e89b4973c87a138dbe21cce41a99
Caleb Hall <caleb.mea.hall@gmail.com>
2022-01-19 13:19:50 -0700

    Removed debuging code

--- src/main.rs
+++ src/main.rs
@@ -32,79 +32,12 @@ fn main() {
     let difficulty: u32 = difficulty.trim().parse().expect("Please type a number.");
 
     generate_maze(difficulty);
-    
-    // svg_test();
 }
-
-// fn svg_test() {
-//     let mut data = Data::new()
-//         .move_to((20, 10))
-//         .line_by((0, 50))
-//         .line_by((50, 0))
-//         .line_by((0, -50))
-//         .close();
-
-//     {
-//         data = data.move_to((35,35))
-//             .line_by((0,10))
-//             .close();
-//     }
-
-//     let path = Path::new()
-//         .set("fill", "none")
-//         .set("stroke", "black")
-//         .set("stroke-width", 1)
-//         .set("d", data);
-    
-//     let document = Document::new()
-//         .set("viewBox", (0, 0, 100, 200))
-//         .add(path);
-
-//     svg::save("image.svg", &document).unwrap();
-// }
 
 fn generate_maze(difficulty: u32){
     let mut grid = create_empty_grid(difficulty);
     
     generate_with_recusive_randomized_depth_first(&mut grid);
-
-    // for row in 0..grid.len() {
-    //     for cell_row in 0..3{
-    //         for column in 0..grid[row].len() {
-    //             if cell_row == 0{
-    //                 print!("x");
-    //                 if grid[row][column][0]{
-    //                     print!("x");
-    //                 } else {
-    //                     print!(" ");
-    //                 }
-    //                 print!("x");
-    //             } else if cell_row == 1{
-    //                 if grid[row][column][3]{
-    //                     print!("x");
-    //                 } else {
-    //                     print!(" ");
-    //                 }
-    //                 print!(" ");
-    //                 if grid[row][column][1]{
-    //                     print!("x");
-    //                 } else {
-    //                     print!(" ");
-    //                 }
-                    
-    //             } else {
-    //                 print!("x");
-    //                 if grid[row][column][2]{
-    //                     print!("x");
-    //                 } else {
-    //                     print!(" ");
-    //                 }
-    //                 print!("x");
-    //             }
-    //         }
-    //         println!("");
-    //     }
-    // }
 
     create_svg(grid, difficulty);
 }
@@ -130,27 +63,22 @@ fn create_svg(grid: Vec<Vec<[bool; 5]>>, difficulty: u32) {
                 data = data.move_to((top_left_y, top_left_x))
                     .line_by((100, 0))
                     .close();
-                //println!("Created line, {}", grid[row][column][0]);
             }
             if grid[row][column][1]{
                 data = data.move_to((top_left_y + 100, top_left_x))
                     .line_by((0, 100))
                     .close();
-                //println!("Created line, {}", grid[row][column][1]);
             }
             if grid[row][column][2]{
                 data = data.move_to((top_left_y, top_left_x + 100))
                     .line_by((100, 0))
                     .close();
-                //println!("Created line, {}", grid[row][column][2]);
             }
             if grid[row][column][3]{
                 data = data.move_to((top_left_y, top_left_x))
                     .line_by((0, 100))
                     .close();
-                //println!("Created line, {}", grid[row][column][3]);
             }
-
         }
     }
     
@@ -185,28 +113,12 @@ fn rdf(grid: &mut Vec<Vec<[bool; 5]>>, cell_x: u32, cell_y: u32) {
         grid[cell_x][cell_y][4] = true;
     }
     while adj_unvisited[4] {
-        //println!("Moved to cell {}/{}-------------------------------------------------------------------------------", cell_x, cell_y);
-        //println!("visited cells Top: {}, Right: {}, Bottom: {}, Left: {}", adj_unvisited[0], adj_unvisited[1], adj_unvisited[2], adj_unvisited[3]);
-        //for row in 0..grid.len() {
-        //    for column in 0..grid[row].len() {
-        //        if grid[row][column][4]{
-        //            print!("x");
-        //        }
-        //        else{
-        //            print!("o");
-        //        }
-        //    }
-        //    println!("")
-        //}
-
+        
         let mut selected_cell: usize = rand::thread_rng().gen_range(0..4);
         
         while adj_unvisited[selected_cell] {
-            //println!("Checked cell: {}", selected_cell);
             selected_cell = rand::thread_rng().gen_range(0..4);
         }
-
-        //println!("Selected cell: {}", selected_cell);
 
         //Get coords of next cell.
         let mut cell_x: i32 = cell_x.try_into().unwrap();
@@ -221,11 +133,9 @@ fn rdf(grid: &mut Vec<Vec<[bool; 5]>>, cell_x: u32, cell_y: u32) {
             let mut next_cell_x: usize = next_cell_x.try_into().unwrap();
             let mut next_cell_y: usize = next_cell_y.try_into().unwrap();
             //Open wall in this cell.
-            //println!("-------Opened wall {} on this cell({}:{}).----------", selected_cell, cell_x, cell_y);
             grid[cell_x][cell_y][selected_cell] = false;
             //Open wall in next cell.
             let next_selected_wall = (selected_cell + 2) % 4;
-            //println!("-------Opened wall {} on next cell({}:{}).----------", next_selected_wall, next_cell_x, next_cell_y);
             grid[next_cell_x][next_cell_y][next_selected_wall] = false;
         }
         //Call rdf on next cell
@@ -235,7 +145,6 @@ fn rdf(grid: &mut Vec<Vec<[bool; 5]>>, cell_x: u32, cell_y: u32) {
         let mut cell_x: u32 = cell_x.try_into().unwrap();
         let mut cell_y: u32 = cell_y.try_into().unwrap();
         adj_unvisited = count_adj_unvisited(grid, cell_x, cell_y);
-        //println!("++++++++++++++++++++++++++++++++++++++++++++++++++++++++Backed up");
     }
 }
 
@@ -250,7 +159,6 @@ fn count_adj_unvisited(grid: &mut Vec<Vec<[bool; 5]>>, cell_x: u32, cell_y: u32)
 
     //cell up
     if cell_x > 0 {
-        //print!("{}, ", grid[cell_x - 1][cell_y][4]);
         if grid[cell_x - 1][cell_y][4] == false{
             adj_unvisited = true;
             cell_up = false;
@@ -258,7 +166,6 @@ fn count_adj_unvisited(grid: &mut Vec<Vec<[bool; 5]>>, cell_x: u32, cell_y: u32)
     }
     //cell right
     if (cell_y + 1) < grid[1].len().try_into().unwrap() {
-        //print!("{}, ",grid[cell_x][cell_y + 1][4]);
         if grid[cell_x][cell_y + 1][4] == false{
             adj_unvisited = true;
             cell_right = false;
@@ -266,7 +173,6 @@ fn count_adj_unvisited(grid: &mut Vec<Vec<[bool; 5]>>, cell_x: u32, cell_y: u32)
     }
     //cell down
     if (cell_x + 1) < grid.len().try_into().unwrap() {
-        //print!("{}, ", grid[cell_x + 1][cell_y][4]);
         if grid[cell_x + 1][cell_y][4] == false{
             adj_unvisited = true;
             cell_down = false;
@@ -274,7 +180,6 @@ fn count_adj_unvisited(grid: &mut Vec<Vec<[bool; 5]>>, cell_x: u32, cell_y: u32)
     }
     //cell left
     if cell_y > 0 {
-        //println!("{} ", grid[cell_x][cell_y - 1][4]);
         if grid[cell_x][cell_y - 1][4] == false{
             adj_unvisited = true;
             cell_left = false;
@@ -303,22 +208,6 @@ fn create_empty_grid(difficulty: u32) -> Vec<Vec<[bool; 5]>> {
         grid.push(row_vec)
     }
 
-    //Show the grid and the first wall in each cell, this is for testing.
-    // for row in &grid {
-    //     for column in row {
-    //         print!("{}", column[0]);
-    //     }
-    //     println!("")
-    // }
-
-    // println!("--------------------------------------------");
-    
-    // for row in 0..grid.len() {
-    //     for column in 0..grid[row].len() {
-    //         print!("{}", grid[row][column][0]);
-    //     }
-    //     println!("")
-    // }
-
     grid
 }
+//324
